09c678b1ef5181f864cc9a3fafdd9aa80a469926
erika mayoute <e.mayoute44@gmail.com>
2022-11-07 12:37:13 +0100

    wip dropdown menu js

--- hyperlux/script.js
+++ hyperlux/script.js
@@ -1,1 +1,16 @@
 console.log('connecté!');
+
+// Data for menu
+var data = {
+  "menu":[
+    {
+      "name": "Node 1", "link": "#", "submenu": [
+        { "name": "economie du luxe", "link": "#", "submenu": null },
+        { "name": "rencontre", "link": "#", "submenu": null },
+        { "name": "services", "link": "#", "submenu": null },
+        { "name": "success story", "link": "#", "submenu": null },
+        { "name": "best of luxe", "link": "#", "submenu": null }
+      ]
+    }
+  ]
+};
