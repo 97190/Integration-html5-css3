7d1f8639d76b14337fc69655523aea8ee4068f6e
erika mayoute <e.mayoute44@gmail.com>
2022-11-08 15:28:20 +0100

    test javascript

--- hyperlux/script.js
+++ hyperlux/script.js
@@ -1,16 +1,7 @@
 console.log('connecté!');
 
-// Data for menu
-var data = {
-  "menu":[
-    {
-      "name": "Node 1", "link": "#", "submenu": [
-        { "name": "economie du luxe", "link": "#", "submenu": null },
-        { "name": "rencontre", "link": "#", "submenu": null },
-        { "name": "services", "link": "#", "submenu": null },
-        { "name": "success story", "link": "#", "submenu": null },
-        { "name": "best of luxe", "link": "#", "submenu": null }
-      ]
-    }
-  ]
-};
+function sum(x, y) {
+  return x + y;
+}
+
+sum (4, 4)
